3b8a123a8ef4927148cb90284189f69a48008c7a
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-05-22 19:10:11 -0300

    Update formulario_salvar.php

--- formulario_salvar.php
+++ formulario_salvar.php
@@ -1,13 +1,12 @@
 <?php
   include "conexao.php";
   
-  $id = $_POST['id'];
-  $nome  = $_POST['nome'];
-  $pass  = $_POST['pass'];
+  $nome  = $_POST['tNome'];
+  $pass  = $_POST['tPass'];
 
    //insercao
    
-  $SQL    = "INSERT INTO user (_id, nome, senha) VALUES  ('_id','$nome','$pass')";
+  $SQL    = "INSERT INTO user (nome, senha) VALUES ($nome','$pass')";
          
   $st = $conexao->prepare($SQL);
 
