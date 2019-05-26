01fbe426a182bc696983f26c4cd6e1dfe51b1b52
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-05-25 21:54:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $("form").submit(function(event){
-	var xmlHttp = new XMLHttpRequest();
+	/*var xmlHttp = new XMLHttpRequest();
 	xmlHttp.onreadystatechange = function() {
 		if(xmlHttp.readyState===4 && xmlHttp.status===200) {
 			document.getElementById("resultado").innerHTML = xmlHttp.responseText;
 		}
 	};
 	xmlHttp.open("POST", "formulario_login.php", true);
-	xmlHttp.send();
+	xmlHttp.send();*/
+	$.ajax({
+		type:'post',
+		dataType:'json',
+		url:'formulario_login.php',
+		success: function(dados){
+			$("#resultado").append(dados);
+		}
+	});
 });
