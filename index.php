1664fe4261b616624aaa1609d51fa9bb5707a586
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-05-26 21:36:38 -0300

    Update index.php
    Adicionei o script diretamente no index

--- index.php
+++ index.php
@@ -36,6 +36,35 @@
 		</footer>
 	</div>
 
-	<script src="indexScript.js"></script>
+	<script>
+		$("form").submit(function(event){
+
+			//Pega as varáveis
+			var vNome = $("#nome").val();
+			var vPass = $("#senha").val();
+
+			//Criando as varáveis
+			var vUrl = "formulario_login.php";
+			var vData = {nome:vNome, senha:vPass};
+
+			$.post({
+				vUrl,
+				vData,
+				function(response,status){
+					if (status == "success") {
+						alert("Funciona!");
+
+						//Pegando dados do json
+						var obj = JQuery.parseJSON(response);
+
+						$("#resultado").html(obj);
+						event.preventDefault();
+					} else {
+						alert("Não Funciona!");
+					}
+				}
+			});
+		});
+	</script>
 </body>
 </html>
