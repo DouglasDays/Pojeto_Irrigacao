d95514213207bfda2acf9e7791b106a3680eca5e
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-05-27 22:26:28 -0300

    Update medicoes.php

--- medicoes.php
+++ medicoes.php
@@ -13,14 +13,16 @@
 		</header>
 
 		<section id="corpo">
-		<label for="var1">var1:</label>
-		<div id="var1"><p>var1</p></div>
+			<fieldset id="retorno">
+				<label for="var1">var1:</label>
+				<div id="var1"><p>var1</p></div>
 
-		<label for="var2">var2:</label>
-		<div id="var2"><p>var2</p></div>
+				<label for="var2">var2:</label>
+				<div id="var2"><p>var2</p></div>
 
-		<label for="var3">var3:</label>
-		<div name="var3" id="var3"><p>var3</p></div>
+				<label for="var3">var3:</label>
+				<div name="var3" id="var3"><p>var3</p></div>
+			</fieldset>
 		</section>
 
 		<footer id="rodape">
