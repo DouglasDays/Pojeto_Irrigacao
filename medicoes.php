56811ab57e9afac585a6f170827f091c26a23766
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-05-27 21:50:43 -0300

    Update medicoes.php
    Adicionando diva para interface

--- medicoes.php
+++ medicoes.php
@@ -5,6 +5,15 @@
 	<meta charset="utf-8"/>
 </head>
 <body>
-	<p>Olá</p>
+	<div id="interface">
+		<label for="var1">var1</label>
+		<div id="var1"><p>var1</p></div>
+
+		<label for="var2">var2</label>
+		<div id="var2"><p>var2</p></div>
+
+		<label for="var3">var3</label>
+		<div id="var3"><p>var3</p></div>
+	</div>
 </body>
 </html>
