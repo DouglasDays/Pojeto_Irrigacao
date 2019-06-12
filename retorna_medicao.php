e3e2f5efb61677889bd72c1487b94122076f6f90
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-12 12:30:37 -0300

    Update retorna_medicao.php

--- retorna_medicao.php
+++ retorna_medicao.php
@@ -19,7 +19,7 @@
 
 	//Se a query executar guarda os dados em um array e retorna
 	if ($st->execute()) {
-		if ($st->rowCount(); > 0) { //Se der erro verificar esta linha!
+		if ($st->rowCount() > 0) { //Se der erro verificar esta linha!
 			while ($data = $st->fetch(PDO::FETCH_ASSOC)) {
 				$retorno['id'] = $data['_id'];
 				$retorno['vu'] = $data['vu'];
