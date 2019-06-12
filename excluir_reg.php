76a3ef6a27b5958d9901196ff5dd399b42f2026a
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-12 17:22:40 -0300

    Update excluir_reg.php

--- excluir_reg.php
+++ excluir_reg.php
@@ -2,7 +2,7 @@
 	include 'conexao.php';
 
 	//Deleta todos os dados da tabela deixando apenas os 5 ultimos
-	$SQL = "DELETE FROM medicao WHERE _id = (SELECT _id FROM medicao ORDER BY _id DESC LIMIT 5)";
+	$SQL = "DELETE FROM medicao WHERE _id IN (SELECT _id FROM medicao ORDER BY _id DESC LIMIT 5)";
 
 	//Prepara a query para ser executada
 	$st = $conexao->prepare($SQL);
