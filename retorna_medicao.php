913db073daadf20bdd80599dbe2687787baab84d
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-12 12:11:25 -0300

    Update retorna_medicao.php

--- retorna_medicao.php
+++ retorna_medicao.php
@@ -15,6 +15,7 @@
 			$SQL_DELETE = "DELETE FROM medicao WHERE _id NOT IN (SELECT _id FROM medicao ORDER BY _id LIMIT 5)";
 
 			$stm = $conexao->prepare($SQL_DELETE);
+			
 			if ($stm->execute()) {
 				echo "olá";
 			}
