78034ba6542725348c8d9f5ac4ca3b642de89efd
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-12 17:41:47 -0300

    Update excluir_reg.php

--- excluir_reg.php
+++ excluir_reg.php
@@ -1,17 +1,19 @@
 <?php
 	include 'conexao.php';
 
-	//Deleta todos os dados da tabela deixando apenas os 5 ultimos
-	$SQL = "DELETE FROM medicao WHERE _id NOT IN (1, 2)";
+	$registros = array();
 
-	//Prepara a query para ser executada
-	$st = $conexao->prepare($SQL);
+	//Deleta todos os dados da tabela deixando apenas os 5 ultimos
+	$SQL = "SELECT _id FROM medicao ORDER BY _id DESC LIMIT 5";
 
 	try {
-		if ($st->execute()) {
-			echo "sql ok";
-		} else echo "sql error";
-	} catch (Exception $e) {
-		#echo $e;
+		$st = $conexao->execute($SQL);
+		while ($data = $st->fetch(PDO::FETCH_ASSOC)) {
+			array_push($registros, $data['_id']);
+		}
+		echo "<pre>",print_r($registros);
+	} catch (PDOexception $e) {
+		echo "Erro: <code>".$e->getMessage()."</code><br/>";
+		echo "algum erro ocorreu!";
 	}
 ?>
