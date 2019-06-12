f889b7d11023529f5b533881f7311d7935c9c680
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-12 16:38:19 -0300

    Update excluir_reg.php

--- excluir_reg.php
+++ excluir_reg.php
@@ -2,14 +2,17 @@
 	include 'conexao.php';
 
 	//Deleta todos os dados da tabela deixando apenas os 5 primeiros
-	$SQL = "DELETE FROM medicao WHERE _id NOT IN (SELECT _id FROM medicao ORDER BY _id LIMIT 5)";
+	$SQL = "SELECT _id FROM medicao ORDER BY _id LIMIT 5";
 
 	//Prepara a query para ser executada
 	$st = $conexao->prepare($SQL);
 
 	try {
-		$st->execute();
+		if ($st->execute();) {
+			echo "insert ok";
+		} else echo "insert error";
+		
 	} catch (Exception $e) {
-		echo "$e";
+		echo $e;
 	}
 ?>
