8eacc26f49b20a1cbfd1c141c4f521c4b05fbdc1
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-13 16:13:53 -0300

    Update excluir_reg.php

--- excluir_reg.php
+++ excluir_reg.php
@@ -3,25 +3,13 @@
 
 	//SELECT * FROM tabela WHERE id NOT IN (SELECT id FROM tabela ORDER BY id LIMIT 10)
 
-	$SQL = "SELECT * FROM medicao ORDER BY _id";
+	$SQL = "DELETE FROM medicao WHERE _id NOT IN (SELECT _id FROM medicao ORDER BY _id LIMIT 10)"
 
-	$st = $conexao->prepare();
+	$stm = $conexao->prepare();
 
-	if ($st->execute()) {
-		if ($st->rowCount() > 10) {
-			$SQL = "DELETE FROM medicao WHERE _id NOT IN (SELECT _id FROM medicao ORDER BY _id LIMIT 10)"
-
-			$stm = $conexao->prepare();
-
-			try {
-				$stm->execute();
-			} catch (Exception $e) {
-				echo $e->getMessage();
-			}
-		} else {
-			echo "dados não encontrados";
-		}
-	} else {
-		echo "query error";
+	try {
+		$stm->execute();
+	} catch (Exception $e) {
+		echo $e->getMessage();
 	}
 ?>
