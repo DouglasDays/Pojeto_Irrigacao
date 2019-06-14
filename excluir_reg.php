63e7cf426051f794d67cd808ae01d2d3f4e1c800
DouglasDays <50713487+DouglasDays@users.noreply.github.com>
2019-06-13 23:09:32 -0300

    Update excluir_reg.php

--- excluir_reg.php
+++ excluir_reg.php
@@ -2,6 +2,21 @@
 	include 'conexao.php';
 
 	//SELECT * FROM tabela WHERE id NOT IN (SELECT id FROM tabela ORDER BY id LIMIT 10)
+
+	/*$pessoa = array();
+	$sql='SELECT nomes FROM usuarios';
+							
+ 	try{
+     	$query = $conecta->query($sql); // Pra selects use direto query não precisa preparar antes
+     	while($ln = $query->fetchAll(PDO::FETCH_ASSOC)):
+        	array_push($pessoas, $ln['nome']);
+     	endwhile;
+     	echo '<pre>', print_r($pessoas);
+    }catch(PDOexception $e){
+        echo "Erro: <code>".$e->getMessage()."</code><br />"; // Procure exibir o erro pra poder corrigir
+        echo 'Não deu';
+	}*/
+
 	$SQL = "SELECT _id FROM medicao ORDER BY _id DESC LIMIT 10";
 
 	$st = $conexao->prepare($SQL);
@@ -9,15 +24,13 @@
 	if ($st->execute()) {
 		if($st->rowCount() > 0) { //Verifica se o select retornou alguma linha
 
-			while ($dados[] = $st->fetch(PDO::FETCH_ASSOC)) { //guarda os dados da query
-				for ($i=0; $i < 10; $i++) { 
-					$retorno = $dados;
-					settype($retorno, "string");
-					$SQL .= $retorno.", ";
-				}
+			while ($data = $st->fetchAll(PDO::FETCH_ASSOC)) { //guarda os dados da query 
+				array_push($retorno, $data['_id']);
 			}
 
-			$SQL = "DELETE FROM medicao WHERE _id NOT IN ("; //deleta os dados que não sejam aqueles retornados
+			$SQL = "DELETE FROM medicao WHERE _id NOT IN (:R)"; //deleta os dados que não sejam aqueles retornados
+
+			$st->bindParam(":R", $retorno);
 
 			var_dump($SQL);
 
